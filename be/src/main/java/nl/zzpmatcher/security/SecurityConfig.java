fead844153179b1a1fd076ebae6ba2c4305fca3b
Marc Enschede <marc@Marcs-MBP-2.fritz.box>
2017-06-10 20:19:59 +0200

    Fix

--- be/src/main/java/nl/zzpmatcher/security/SecurityConfig.java
+++ be/src/main/java/nl/zzpmatcher/security/SecurityConfig.java
@@ -1,23 +1,13 @@
 package nl.zzpmatcher.security;
 
-import com.sun.tools.javac.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
