cc28566a0ca51017a5749d021b2c4e6da2b00f29
JWA van Golverdinge Schut <Jaron.van.Golverdinge.Schut@rabobank.nl>
2017-09-14 21:16:30 +0200

    Added the storeFreez for development

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -8,12 +8,15 @@ import {LoginService, ProfileService, RegistrationService} from '../service/inde
 import {AccountPageComponent, LoginPageComponent, WelcomePageComponent} from '../page/index';
 import {LoginComponent, RegistrationComponent} from '../component/index';
 import {appRoutingProviders, routing} from './app.routes';
+import {compose} from '@ngrx/core';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 import {LoginEffectsService} from '../store/effects/login-effects.service';
 import {RegistrationEffectsService} from '../store/effects/registration-effects.service';
 import {application} from '../store/application-reducer';
 import {INITIAL_APPLICATION_STATE} from '../store/application-state';
+import {storeFreeze} from 'ngrx-store-freeze';
+import {environment} from '../environments/environment';
 
 const PROVIDERS = [
   appRoutingProviders,
@@ -23,7 +26,11 @@ const PROVIDERS = [
 ];
 
 export function applicationReducer(state: any = INITIAL_APPLICATION_STATE, action: any) {
-  return application(state, action);
+  const combinedReducers = application(state, action);
+  if (environment.production) {
+    return combinedReducers;
+  }
+  return compose(storeFreeze, combinedReducers);
 }
 
 @NgModule({
